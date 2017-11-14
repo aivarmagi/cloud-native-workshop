3d82e9f8c2441caac8474b7afa5d8877b185efd8
Aivar Mägi <aivar.magi@partner.smit.ee>
2017-11-14 10:24:38 +0200

    add redis session support

--- 12f_app/src/main/java/com/github/aivarmagi/app/StockSpringBootStarterS.java
+++ 12f_app/src/main/java/com/github/aivarmagi/app/StockSpringBootStarterS.java
@@ -3,7 +3,9 @@ package com.github.aivarmagi.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class StockSpringBootStarterS {
     public static void main(String[] args) {
